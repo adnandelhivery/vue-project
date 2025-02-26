139dc6d2b63ecabff7d835fc7b5af74132f52753
Adnan Khan <adnan.khan2@delhivery.com>
2025-02-26 10:55:18 +0530

    implement todos

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,75 @@
+<!-- composition api -->
+
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, ref } from "vue";
+
+const name = ref("Adnan Khan");
+const status = ref("active");
+const tasks = ref(["Task1", "Task2", "Task3"]);
+const newTask = ref("");
+
+const toggleStatus = () => {
+  if (status.value === "active") {
+    status.value = "pending";
+  } else if (status.value === "pending") {
+    status.value = "inactive";
+  } else {
+    status.value = "active";
+  }
+};
+
+const addTask = () => {
+  if (newTask.value.trim() !== "") {
+    tasks.value.push(newTask.value);
+    newTask.value = "";
+  }
+};
+
+const deleteTask = (index) => {
+  tasks.value.splice(index, 1);
+};
+
+onMounted(async () => {
+  try {
+    const res = await fetch("https://jsonplaceholder.typicode.com/todos");
+    const data = await res.json();
+    console.log(data);
+
+    tasks.value = data.map((task) => task.title);
+  } catch (err) {
+    console.log(err);
+  }
+});
 </script>
 
 <template>
-  <div>
-    <a href="https://vite.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
-  </div>
-  <HelloWorld msg="Vite + Vue" />
-</template>
+  <h1 class="">{{ name }}</h1>
+  <p v-if="status === 'active'">User is {{ status }}</p>
+  <p v-else-if="status === 'pending'">User is {{ status }}</p>
+  <p v-else>User is {{ status }}</p>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+  <form @submit.prevent="addTask">
+    <label for="newTask">Add Task</label>
+    <input type="text id" id="newTask" name="newTask" v-model="newTask" />
+    <button type="submit">Add Task</button>
+  </form>
+
+  <h3>Tasks:</h3>
+  <ul class="">
+    <li v-for="(task, index) in tasks" :key="index">
+      <span>
+        {{ task }}
+      </span>
+      <button @click="deleteTask(index)">X</button>
+    </li>
+  </ul>
+  <br />
+  <!-- <a v-bind:href="link">Click for Google</a>  v-bind directive -->
+
+  <!-- //alternate way to do the same thing -->
+  <a :href="link">Click for Google</a>
+
+  <!-- <button v-on:click="toggleStatus">change status</button> handling event -->
+  <button @click="toggleStatus">change status</button>
+  <!-- alternate way to do the same thing -->
+</template>
---
+++ src/App2.vue
@@ -0,0 +1,50 @@
+<!-- options API -->
+<script>
+export default{
+  data(){
+    return{
+      name:"Adnan Khan",
+      status:"active",
+      tasks:["Task1","Task2","Task3"],
+      link:"https://www.google.com"
+    };
+  },
+
+
+methods:{
+    toggleStatus(){
+      if(this.status === "active"){
+        this.status = "pending";
+    }
+    else if (this.status === "pending"){
+      this.status = "inactive";
+  } 
+   else{
+    this.status = "active";
+  }
+},
+},
+};
+
+
+</script>
+
+<template>
+  <h1 class="">{{ name }}</h1>
+  <p v-if="status === 'active'">User is {{ status }}</p>
+  <p v-else-if="status === 'pending'">User is  {{ status }}</p>
+  <p v-else>User is {{ status }}</p>
+
+  <h3>Tasks:</h3>
+  <ul class="">
+    <li v-for="task in tasks":key="task">{{ task }}</li>
+  </ul>
+<br/>
+  <!-- <a v-bind:href="link">Click for Google</a>  v-bind directive -->
+  
+  <!-- //alternate way to do the same thing -->
+  <a :href="link">Click for Google</a> 
+
+  <!-- <button v-on:click="toggleStatus">change status</button> handling event -->
+  <button @click="toggleStatus">change status</button>  <!-- alternate way to do the same thing -->
+</template>
